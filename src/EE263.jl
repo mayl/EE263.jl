6597c081ecc176dec7c84e146460e73971328648
Larry May <larrymayster@gmail.com>
2021-09-08 18:09:38 -0400

    added plots

--- src/EE263.jl
+++ src/EE263.jl
@@ -2,4 +2,15 @@ module EE263
 
 # Write your package code here.
 
+export name
+export greeting
+
+function greeting()
+    println("Hello, World!")
+end
+
+function name()
+    println("Larry")
+end
+
 end
